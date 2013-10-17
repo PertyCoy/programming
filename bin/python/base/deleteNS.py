c6cd29607aa05ee69998c6ab37232f7a1ae2ca43
unknown <EriLee@.bigbigsun.com>
2013-10-17 17:12:28 +0800

    Deleted scenes namespace

---
+++ bin/python/base/deleteNS.py
@@ -0,0 +1,11 @@
+import maya.cmds as cmds
+
+
+def deleteNS():
+    sel_namespace = cmds.ls(sl=True)[0]
+    NS = sel_namespace.rsplit(':',1)[0]
+    if not cmds.namespace(ex=NS):
+        raise Exception('Namespace "'+NS+'" does not exist!')
+    cmds.namespace(mv=[NS,':'],f=True)
+    cmds.namespace(rm=NS)
+
